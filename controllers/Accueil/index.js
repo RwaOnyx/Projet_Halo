ce4cf392eb2eae8f7291572e8450ca0b5e7ed4c0
RwaOnyx <ewan.girodon01@gmail.com>
2023-10-27 00:51:35 +0000

    css accueil

--- controllers/Accueil/index.js
+++ controllers/Accueil/index.js
@@ -1,3 +1,57 @@
+import query from '../../database.js';
+import { DateTime } from "luxon"; 
+
 export default (req, res) => {
-    res.render('index.ejs');
+    query(
+        `SELECT
+    fs.id AS id,
+    fs.intitule AS intitule,
+    fs.date AS date,
+    u.login AS login,
+    u.image AS image,
+    CASE
+        WHEN MAX(fm.date) > fs.date THEN MAX(fm.date)
+        ELSE fs.date
+    END AS derniereUpdate,
+    COUNT(fm.id) AS nbMessages
+FROM ForumSalon fs
+INNER JOIN Users u ON fs.idUtilisateur = u.id
+LEFT JOIN ForumMessage fm ON fs.id = fm.idSalon
+GROUP BY fs.id, fs.intitule, fs.date, u.login
+ORDER BY derniereUpdate DESC
+LIMIT 1;`,
+        (error, salonJour) => {
+            if (error) {
+                console.error(`Erreur lors de l'exécution de la requête ${error}`);
+                res.status(500).send('Erreur serveur');
+                return;
+            }
+            
+            salonJour=salonJour[0]
+            let dateISO = (salonJour.date).toISOString();
+            let dateLuxon = DateTime.fromISO(dateISO);
+            salonJour.date = dateLuxon.toFormat('dd/LL/yyyy HH:mm');
+            
+            query(
+        `SELECT message
+        FROM ForumMessage
+        WHERE idSalon=?
+ORDER BY date
+LIMIT 1;`,salonJour.id,
+        (error, messageSalonJour) => {
+            if (error) {
+                console.error(`Erreur lors de l'exécution de la requête ${error}`);
+                res.status(500).send('Erreur serveur');
+                return;
+            }
+            salonJour.premierMessage = messageSalonJour[0].message
+            
+            res.render('index.ejs', { salonJour });
+        
+            
+       } )
+
+
+        })
 };
+
